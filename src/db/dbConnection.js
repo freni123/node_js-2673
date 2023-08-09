d66ed5f30ca1195cdacf3f7b50daacc90f8ae217
freni123 <frenivaghasiya@gmail.com>
2023-08-09 21:45:45 +0530

    09/08/23

---
+++ src/db/dbConnection.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const config = require("../config/config");
+
+const coonectDB = async () =>{
+    mongoose
+    .connect(config.mongodb.url,config.mongodb.options)
+    .then((data) => {
+        console.log("Database Connection Successfully connected!");
+    })
+    .catch((error) =>{
+        console.log("Database Connection error:",error);
+    });
+};
+module.exports = { connectDB };
